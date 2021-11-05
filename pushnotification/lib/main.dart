281935b4356bf5be6f4d08613a6969165fa2dae8
huuthang28 <huuthang28.work@gmail.com>
2021-11-05 21:47:16 +0700

    local notification
    notification flutter

---
+++ pushnotification/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'notifications.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner:false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Notifications _notifications =  Notifications();
+
+  @override
+  void initState() {
+    super.initState();
+    this._notifications.initNotifications();
+  }
+
+  void _pushNotification() {
+    this._notifications.pushNotification();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pushNotification,
+        tooltip: 'Push notifications',
+        child: Icon(Icons.notifications),
+      ),
+    );
+  }
+}
